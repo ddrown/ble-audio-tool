a4392c085c7d7c8f33a93f0698fedb97567cfd48
Dan Drown <dan-android@drown.org>
2025-11-07 00:13:28 -0600

    implement chosing a file

--- src/main.js
+++ src/main.js
@@ -1,29 +1,48 @@
 import elc3 from "./elc3mod.js";
 
+const input = document.createElement("input");
+input.type = "file";
+input.accept = ".wav,audio/wav";
+let inputFileData = undefined;
+input.onchange = (event) => {
+  const file = event.target.files[0]; // Get the first selected file
+  document.getElementById("filename").textContent = file.name;
+
+  if (file) {
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+        const arrayBuffer = e.target.result; // Get the ArrayBuffer
+        inputFileData = new Uint8Array(arrayBuffer); // Create Uint8Array
+    };
+
+    reader.onerror = function(e) {
+        console.error("Error reading file:", e.target.error);
+    };
+
+    reader.readAsArrayBuffer(file); // Start reading the file as an ArrayBuffer
+  }
+}
+
+document.getElementById("clear").addEventListener("click", () => {
+  inputFileData = undefined;
+  document.getElementById("filename").textContent = "";
+});
+
 document.querySelector("#convert-btn").addEventListener("click", (event) => {
   event.preventDefault();
   const Module = {
-    /*
-    printErr: (function () {
-      const element = document.getElementById('stderr');
-      if (element) element.value = ''; // clear browser cache
-      return (...args) => {
-        const text = args.join(' ');
-        // These replacements are necessary if you render to raw HTML
-        //text = text.replace(/&/g, "&amp;");
-        //text = text.replace(/</g, "&lt;");
-        //text = text.replace(/>/g, "&gt;");
-        //text = text.replace('\n', '<br>', 'g');
-        console.log(text);
-        if (element) {
-          element.value += text + "\n";
-          element.scrollTop = element.scrollHeight; // focus on bottom
-        }
-      };
-    })(),
-    */
-    arguments: ["-b", "160000", "example-audio/11ff_.48k.wav", "11ff_.lc3"],
+    arguments: ["-b", "160000", "/input.wav", "/output.lc3"],
+    preRun: () => {
+      if (inputFileData !== undefined) {
+        Module.FS.writeFile("/input.wav", inputFileData);
+      }
+    },
   };
 
   elc3(Module);
+});
+
+document.querySelector("#upload").addEventListener("click", () => {
+  input.click();
 });
